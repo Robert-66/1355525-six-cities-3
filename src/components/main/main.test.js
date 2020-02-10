3d30255859de8da4d08e93ed28982a023f4e71a9
Robert Krepets <robert.krepets@megafon.ru>
2020-02-10 13:55:09 +0500

    Add snapshot tests

---
+++ src/components/main/main.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Main from './main';
+
+const offers = [
+  `Apartments Prinsengracht`,
+  `Lovely Studio With Canal Views`,
+  `Zandberg - Canal view apartments`,
+  `Bright & new apartment with canal view`,
+  `1637: Historic Canal View Suites`
+];
+
+it(`Should Main render correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      offersCount={6}
+      offers={offers}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
