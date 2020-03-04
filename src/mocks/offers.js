31f9b8350917d40c9a8d2d231c565e92cfd835d4
Robert Krepets <robert.krepets@megafon.ru>
2020-03-04 12:03:40 +0500

    Add mocks for offers card

---
+++ src/mocks/offers.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 11,
+    name: `Apartments Prinsengracht`,
+    price: 120,
+  },
+  {
+    id: 21,
+    name: `Lovely Studio With Canal Views`,
+    price: 99,
+  },
+  {
+    id: 31,
+    name: `Zandberg - Canal view apartments`,
+    price: 140,
+  },
+  {
+    id: 41,
+    name: `Bright & new apartment with canal view`,
+    price: 125,
+  }
+];
