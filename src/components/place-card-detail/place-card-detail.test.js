b0b68c5241f85ad1d70248b05a31ec754530cbbc
Robert Krepets <robert.krepets@megafon.ru>
2020-03-26 22:59:52 +0500

    Update tests

--- src/components/place-card-detail/place-card-detail.test.js
+++ src/components/place-card-detail/place-card-detail.test.js
@@ -1,46 +1,142 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom';
 import {CityNames} from '../../const';
 import {PlaceCardDetail} from './place-card-detail';
 
-const offer = {
-  data: {
-    city: {
-      name: CityNames.AMSTERDAM,
-      location: {
-        latitude: 52.37454,
-        longitude: 4.897976,
-        zoom: 13
+const mocks = {
+  offer: {
+    data: {
+      city: {
+        name: CityNames.AMSTERDAM,
+        location: {
+          latitude: 52.37454,
+          longitude: 4.897976,
+          zoom: 13
+        }
+      },
+      description: `description`,
+      bedrooms: 2,
+      maxAdults: 3,
+      goods: [`Air conditioning`, `Washer`, `Dishwasher`],
+      images: [`img/image1.jpg`, `img/image2.jpg`],
+      isFavorite: false,
+      previewImage: `img/apartment-01.jpg`,
+      isPremium: false,
+      type: `apartment`,
+      id: 11,
+      name: `Apartments Prinsengracht`,
+      price: 120,
+      coords: [52.3909553943508, 4.85309666406198],
+      rating: 4.3,
+      host: {
+        name: `Angelina`,
+        isPro: true,
+        avatarUrl: `img/avatar-angelina.jpg`,
+      },
+    },
+    isLoading: false,
+    isError: false,
+  },
+  offersNearby: [
+    {
+      city: {
+        name: CityNames.AMSTERDAM,
+        location: {
+          latitude: 52.37454,
+          longitude: 4.897976,
+          zoom: 13
+        }
+      },
+      previewImage: `img/apartment-01.jpg`,
+      isPremium: true,
+      type: `apartment`,
+      id: 21,
+      name: `Lovely Studio With Canal Views`,
+      price: 99,
+      coords: [52.369553943508, 4.85309666406198],
+      rating: 3.1,
+    },
+    {
+      city: {
+        name: CityNames.AMSTERDAM,
+        location: {
+          latitude: 52.37454,
+          longitude: 4.897976,
+          zoom: 13
+        }
+      },
+      previewImage: `img/apartment-01.jpg`,
+      isPremium: true,
+      type: `apartment`,
+      id: 31,
+      name: `Zandberg - Canal view apartments`,
+      price: 140,
+      coords: [52.3909553943508, 4.929309666406198],
+      rating: 5,
+    },
+    {
+      city: {
+        name: CityNames.AMSTERDAM,
+        location: {
+          latitude: 52.37454,
+          longitude: 4.897976,
+          zoom: 13
+        }
+      },
+      previewImage: `img/apartment-01.jpg`,
+      isPremium: false,
+      type: `room`,
+      id: 41,
+      name: `Bright & new apartment with canal view`,
+      price: 125,
+      coords: [52.3809553943508, 4.939309666406198],
+      rating: 4.9,
+    },
+  ],
+  reviews: [
+    {
+      comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      date: `2019-05-08T14:13:56.569Z`,
+      id: 1,
+      rating: 3,
+      user: {
+        avatarUrl: `img/1.png`,
+        name: `Max`,
       }
     },
-    description: `description`,
-    bedrooms: 2,
-    maxAdults: 3,
-    goods: [`Air conditioning`, `Washer`, `Dishwasher`],
-    images: [`img/image1.jpg`, `img/image2.jpg`],
-    isFavorite: false,
-    previewImage: `img/apartment-01.jpg`,
-    isPremium: false,
-    type: `apartment`,
-    id: 11,
-    name: `Apartments Prinsengracht`,
-    price: 120,
-    coords: [52.3909553943508, 4.85309666406198],
-    rating: 4.3,
-    host: {
-      name: `Angelina`,
-      isPro: true,
-      avatarUrl: `img/avatar-angelina.jpg`,
+    {
+      comment: `A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+      date: `2017-08-08T14:13:56.569Z`,
+      id: 2,
+      rating: 5,
+      user: {
+        avatarUrl: `img/2.png`,
+        name: `Anna`,
+      }
     },
-  },
-  isLoading: false,
-  isError: false,
+  ],
+  currentCityLocation: [52.37454, 4.897976],
 };
+mocks.offersNearbyMap = [mocks.offer.data, ...mocks.offersNearby];
 
 it(`Should PlaceCardDetail render correctly`, () => {
   const tree = renderer
     .create(
-        <PlaceCardDetail offer={offer} />
+        <BrowserRouter>
+          <PlaceCardDetail
+            offer={mocks.offer}
+            offersNearby={mocks.offersNearby}
+            offersNearbyMap={mocks.offersNearbyMap}
+            currentCityLocation={mocks.currentCityLocation}
+            reviews={mocks.reviews}
+          />
+        </BrowserRouter>,
+        {
+          createNodeMock: () => {
+            return document.createElement(`section`);
+          }
+        }
     )
     .toJSON();
 
