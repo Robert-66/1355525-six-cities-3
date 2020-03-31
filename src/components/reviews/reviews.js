e5a080da9dea634baeed3b627f59b3d2cfff06c7
Robert Krepets <robert.krepets@megafon.ru>
2020-03-31 12:09:03 +0500

    Connected to Redux and update Reviews component, update PlaceCardDetail component

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,62 +1,39 @@
 import React from 'react';
 import ReviewsList from '../reviews-list/reviews-list';
+import ReviewsForm from '../reviews-form/reviews-form';
 import {reviewType} from '../../types/reviews-types.js';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {AuthorizationStatus} from '../../reducer/user/user';
+import {Operation} from '../../reducer/data/reviews-form/reviews-form';
+import {ActionCreators} from '../../reducer/data/reviews-form/reviews-form';
 
 function Reviews(props) {
-  const {className, reviews} = props;
+  const {
+    className,
+    reviews,
+    reviewsForm,
+    authorizationStatus,
+    offerId,
+    onSubmitReview,
+    onResetReviewFormState,
+  } = props;
+
+  function handleReviewSubmit(reviewData) {
+    onSubmitReview(offerId, reviewData);
+  }
 
   return (
     <section className={`reviews${className ? ` ` + className : ``}`}>
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ReviewsList reviews={reviews} />
-      <form className="reviews__form form" action="#" method="post">
-        <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating">
-          <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star" />
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star" />
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star" />
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly" >
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star" />
-            </svg>
-          </label>
-
-          <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star" />
-            </svg>
-          </label>
-        </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" />
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
-            To submit review please make sure to set <span className="reviews__star">rating</span> and describe your
-            stay with at least <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
-        </div>
-      </form>
+      {authorizationStatus === AuthorizationStatus.AUTH && (
+        <ReviewsForm
+          reviews={reviewsForm}
+          onSubmit={handleReviewSubmit}
+          onResetState={onResetReviewFormState}
+        />
+      )}
     </section>
   );
 }
@@ -64,6 +41,30 @@ function Reviews(props) {
 Reviews.propTypes = {
   className: PropTypes.string,
   reviews: PropTypes.arrayOf(reviewType).isRequired,
+  reviewsForm: PropTypes.shape({
+    data: PropTypes.arrayOf(PropTypes.object).isRequired,
+    isLoading: PropTypes.bool.isRequired,
+    isError: PropTypes.bool.isRequired,
+  }).isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
+  offerId: PropTypes.number.isRequired,
+  onSubmitReview: PropTypes.func.isRequired,
+  onResetReviewFormState: PropTypes.func.isRequired,
 };
 
-export default Reviews;
+function mapStateToProps(state) {
+  return {
+    authorizationStatus: state.user.authorizationStatus,
+    reviewsForm: state.data.reviewsForm,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onSubmitReview: (offerId, reviewData) => dispatch(Operation.fetchReviewsForm(offerId, reviewData)),
+    onResetReviewFormState: () => dispatch(ActionCreators.resetReviewsFormState()),
+  };
+}
+
+export {Reviews};
+export default connect(mapStateToProps, mapDispatchToProps)(Reviews);
