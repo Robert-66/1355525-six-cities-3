e1cda0bd0611106c18dfcdc48e47224b39a2890f
Robert Krepets <robert.krepets@megafon.ru>
2020-04-06 08:17:01 +0500

    Add reducer, action creators and operation for reviews

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,6 +2,7 @@ import {combineReducers} from 'redux';
 import {reducer as offers} from './data/offers/offers';
 import {reducer as offersFavorite} from './data/offers-favorite/offers-favorite';
 import {reducer as offersNearby} from './data/offers-nearby/offers-nearby';
+import {reducer as reviews} from './data/reviews/reviews';
 import {reducer as reviewsForm} from './data/reviews-form/reviews-form';
 import {reducer as app} from './app/app';
 import {reducer as user} from './user/user';
@@ -11,6 +12,7 @@ export default combineReducers({
     offers,
     offersFavorite,
     offersNearby,
+    reviews,
     reviewsForm,
   }),
   app,
